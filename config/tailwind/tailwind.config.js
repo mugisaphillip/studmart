b90f6112fa8a5dd7ce733a5133b5d20738fd72c7
phillipmugisa <phillipmugisa4@gmail.com>
2024-07-10 00:23:31 +0300

    BUILD: home page layout complete

--- config/tailwind/tailwind.config.js
+++ config/tailwind/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "../../src/templates/*html"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
 
