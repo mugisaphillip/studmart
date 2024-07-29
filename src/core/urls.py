e8b5bf0654f75e027200f9962161408842ae9939
phillipmugisa <phillipmugisa4@gmail.com>
2024-07-30 01:26:58 +0300

    REFACTOR: implementing google auth

--- src/core/urls.py
+++ src/core/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from shop.views import HomeView
 
 urlpatterns = [
     path("", include("shop.urls", namespace="shop")),
     path("auth/", include("identity.urls", namespace="identity")),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('accounts/profile/', include("shop.urls")),
+    path('accounts/profile/', HomeView.as_view()),
 ]
 
 
